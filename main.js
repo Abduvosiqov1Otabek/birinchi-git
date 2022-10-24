fa36f06ec34a98bc46dbb2f006c4ff3b6e391726
Otabek <otabekabduvosiqov09@gmail.com>
2022-10-24 14:09:21 +0500

    birinchi git hub

---
+++ main.js
@@ -0,0 +1,27 @@
+//////////////1-misol//////////////
+// let array = [5, 6, 3, 2, 5, 6, 12]
+// for (let i = 0; i <= array.length-1; i++)
+//  {
+//     if (array[i] % 2 == 1) {
+//         console.log("toq", "[",array[i], "]")
+//     }
+//     else if (array[i] % 2 == 0) {
+//         console.log("juft", "[",array[i], "]")
+//     }
+// }
+//////////////3-misol//////////////
+// let array = ['a', 'b', 'c', 'd', 'e']
+// let index = prompt("Indexni kiriting =")
+// for (let i = 0; i <= array.length-1; i++ ) {
+//     if (i == index) {
+//         continue;
+//     }
+//     else {
+//         console.log(array[i])
+//     }
+// }
+//////////////5-misol//////////////
+// let array = ['a', 'b', 'c', 'd', 'e']
+// for (let i = array.length - 1; i >= 0; i --) {
+//     console.log(("'"), array[i], ("'"))
+// }
